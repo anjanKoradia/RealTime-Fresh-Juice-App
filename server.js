320f456a2d2270355d4ffa92c2604f910b874345
Anjan Koradia <anjankoradia2101@gmail.com>
2021-04-15 19:29:12 +0530

    Create default layout , login page, register page and cart page.

--- server.js
+++ server.js
@@ -8,15 +8,24 @@ const PORT = process.env.PORT || 3000;
 // Assets
 app.use(express.static("public"));
 
-// Routes
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
 // Set template engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 // Start server
 app.listen(PORT, () => {
