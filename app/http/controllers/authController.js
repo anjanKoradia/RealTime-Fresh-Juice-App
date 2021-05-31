9c3bad57c6239d0436664d64bb5d5cc564200da9
Anjan Koradia <anjankoradia2101@gmail.com>
2021-05-31 18:39:27 +0530

    Write locig for register an user in app.

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,3 +1,6 @@
+const User = require("../../models/user");
+const bcrypt = require("bcrypt");
+
 function authController() {
   return {
     login: (req, res) => {
@@ -6,6 +9,51 @@ function authController() {
 
     register: (req, res) => {
       res.render("auth/register");
+    },
+
+    postRegister: async (req, res) => {
+      const { name, email, password } = req.body;
+
+      // Validation for any empty fileld
+      if (!name || !email || !password) {
+        req.flash("error", "All field are required.");
+        req.flash("name", name);
+        req.flash("email", email);
+        return res.redirect("/register");
+      }
+
+      // check if email is alredy exists in database
+      User.exists({ email: email }, (error, result) => {
+        if (result) {
+          req.flash("error", "Email is already taken.");
+          req.flash("name", name);
+          req.flash("email", email);
+          return res.redirect("/register");
+        }
+      });
+
+      // Encode password (Hase Password)
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create user in database
+      const user = new User({
+        name: name,
+        email: email,
+        password: hashedPassword,
+      });
+
+      // save user in database
+      user
+        .save()
+        .then((user) => {
+          // Login
+
+          return res.redirect("/");
+        })
+        .catch((error) => {
+          req.flash("error", "Somthing went wrong.");
+          return res.redirect("/register");
+        });
     },
   };
 }
---
+++ app/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+    role: { type: String, default: "customer" },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
