a7a9c43ac7960116bc9782161d9a8d962c0fdc44
Anjan Koradia <anjankoradia2101@gmail.com>
2021-10-11 23:03:37 +0530

    Image drag & drop functionality during add new juice

--- resources/js/menu.js
+++ resources/js/menu.js
@@ -2,7 +2,46 @@ const imgDropZone = document.querySelector(".drop_img_container");
 const browseBtn = document.querySelector(".browse_btn");
 const fileInput = document.querySelector("#fileInput");
 
+function updateThumbnail(file) {
+  let thumbnailElement = imgDropZone.querySelector(".drop_zone_thumb");
+
+  if (imgDropZone.querySelector("i") && imgDropZone.querySelector("p")) {
+    imgDropZone.querySelector("i").remove();
+    imgDropZone.querySelector("p").remove();
+  }
+
+  if (!thumbnailElement) {
+    thumbnailElement = document.createElement("div");
+    thumbnailElement.classList.add("drop_zone_thumb");
+    imgDropZone.appendChild(thumbnailElement);
+  }
+
+  thumbnailElement.dataset.label = file.name;
+
+  // Show thumbnail for image files
+  if (file.type.startsWith("image/")) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+    };
+  } else {
+    thumbnailElement.style.backgroundImage = null;
+  }
+}
+
 export default function addJuice() {
+  browseBtn.addEventListener("click", () => {
+    fileInput.click();
+  });
+
+  fileInput.addEventListener("change", (e) => {
+    if (fileInput.files.length) {
+      updateThumbnail(fileInput.files[0]);
+    }
+  });
+
   imgDropZone.addEventListener("dragover", (e) => {
     e.preventDefault();
     if (!imgDropZone.classList.contains("draged")) {
@@ -10,26 +49,19 @@ export default function addJuice() {
     }
   });
 
-  imgDropZone.addEventListener("dragleave", (e) => {
-    imgDropZone.classList.remove("draged");
+  ["dragleave", "drop"].forEach((type) => {
+    imgDropZone.addEventListener(type, (e) => {
+      imgDropZone.classList.remove("draged");
+    });
   });
 
   imgDropZone.addEventListener("drop", (e) => {
     e.preventDefault();
+
+    if (e.dataTransfer.files.length) {
+      fileInput.files = e.dataTransfer.files;
+      updateThumbnail(e.dataTransfer.files[0]);
+    }
     imgDropZone.classList.remove("draged");
-
-    // let file = e.dataTransfer.files;
-    // fileInput.files = file[0];
-    // if (file.lenght) {
-    //   fileInput.files = file;
-    // }
-  });
-
-  browseBtn.addEventListener("click", () => {
-    fileInput.click();
-  });
-
-  fileInput.addEventListener("change", () => {
-    imgDropZone.innerHTML = `${fileInput.files[0].name} is selected.`;
   });
 }
